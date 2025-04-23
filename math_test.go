227c3fdd3e807a894328e9b2070520fe08e85d87
Bosshentai <baptistamhernani@gmail.com>
2025-04-23 11:16:53 -0100

    feat: :sparkles: Adding function add

---
+++ math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	total := Add(15, 15)
+	if total != 30 {
+		t.Errorf("Resulado da soma é inválido: Resultado: %d, Esperado: %d", total, 30)
+	}
+}
