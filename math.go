dea90701fc57ffeb5b744b1a1fa1b277691d2ca7
Bosshentai <baptistamhernani@gmail.com>
2025-04-23 13:01:37 -0100

    feat: Unpublish add function
    Other packages wont be able to use this function

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Add(1, 2))
+	fmt.Println(add(1, 2))
 }
 
-func Add(a int, b int) int {
+func add(a int, b int) int {
 	return a + b
 }
--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	total := Add(15, 15)
+	total := add(15, 15)
 	if total != 30 {
 		t.Errorf("Resulado da soma é inválido: Resultado: %d, Esperado: %d", total, 30)
 	}
